28807b803a8429bf2bd318ce972adca95860d0b8
SL55ZY <65545295+profsleezy@users.noreply.github.com>
2024-07-15 11:26:50 +0300

    h

--- src/sections/Dashboard/WelcomeBanner.tsx
+++ src/sections/Dashboard/WelcomeBanner.tsx
@@ -16,7 +16,7 @@ const SlideshowContainer = styled.div`
   width: 100%;
   height: 60vh; /* Adjust height as needed */
   overflow: hidden;
-  box-shadow: 1px 7px 10px rgba(0, 0, 0, 0.1);
+  box-shadow: 0px 6px 10px rgba(0, 0, 0, 0.1);
 `;
 
 const BannerImage = styled.img`
