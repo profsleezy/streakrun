f59813247ed0c1981b4fcf0d9f125999462c2f16
SL55ZY <65545295+profsleezy@users.noreply.github.com>
2024-07-15 14:02:31 +0300

    icons

--- src/sections/Dashboard/sidebar.tsx
+++ src/sections/Dashboard/sidebar.tsx
@@ -1,36 +1,74 @@
 import React from 'react';
 import styled from 'styled-components';
+import { ReactComponent as HomeIcon } from './icons/mine.svg';
+
 
 const SidebarContainer = styled.div`
   position: fixed;
   top: 0;
   left: 0;
-  width: 70px; /* Adjust width as needed */
+  width: 70px;
   height: 100vh;
   background-color: #333;
   color: white;
   display: flex;
   flex-direction: column;
-  padding: 1rem;
+  align-items: center;
+  padding: 1rem 0;
   box-shadow: 2px 0 5px rgba(0, 0, 0, 0.1);
 `;
 
 const SidebarItem = styled.div`
-  margin-bottom: 1rem;
+  width: 40px;
+  height: 40px;
+  margin-bottom: 1.5rem;
   cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
   &:hover {
     background-color: #444;
   }
+
+  svg {
+    width: 24px;
+    height: 24px;
+    fill: white;
+  }
+`;
+
+const LowerSection = styled.div`
+  margin-top: auto;
+  padding-bottom: 1rem;
 `;
 
 export function Sidebar() {
   return (
     <SidebarContainer>
-      <SidebarItem>Home</SidebarItem>
-      <SidebarItem>Games</SidebarItem>
-      <SidebarItem>Settings</SidebarItem>
-      <SidebarItem>Profile</SidebarItem>
-      {/* Add more items as needed */}
+      <SidebarItem>
+        <HomeIcon />
+      </SidebarItem>
+      <SidebarItem>
+        <GameIcon />
+      </SidebarItem>
+      <SidebarItem>
+        <SettingsIcon />
+      </SidebarItem>
+      <SidebarItem>
+        <ProfileIcon />
+      </SidebarItem>
+      <SidebarItem>
+        <InfoIcon />
+      </SidebarItem>
+      <LowerSection>
+        <SidebarItem>
+          <HelpIcon />
+        </SidebarItem>
+        <SidebarItem>
+          <LogoutIcon />
+        </SidebarItem>
+      </LowerSection>
     </SidebarContainer>
   );
 }
