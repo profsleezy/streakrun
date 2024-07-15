93d71aaaf5a7ceae6fbcfffa9f03d59292373044
SL55ZY <65545295+profsleezy@users.noreply.github.com>
2024-07-15 04:49:34 +0300

    Update GameCard.tsx

--- src/sections/Dashboard/GameCard.tsx
+++ src/sections/Dashboard/GameCard.tsx
@@ -5,30 +5,37 @@ import styled, { keyframes } from 'styled-components'
 
 const tileAnimation = keyframes`
   0% {
-  background-position: -100px 100px;
+    background-position: -100px 100px;
   }
   100% {
     background-position: 100px -100px;
   }
 `
 
-const StyledGameCard = styled(NavLink)<{$small: boolean, $background: string}>`
+const StyledGameCard = styled(NavLink)<{ $background: string }>`
   width: 100%;
-
-  @media (min-width: 800px) {
-    width: 100%;
-  }
-
-  aspect-ratio: ${(props) => props.$small ? '1/.5' : '1/.6'};
+  aspect-ratio: 1 / 1.5; /* Fixed aspect ratio */
   background-size: cover;
-  
   color: white;
   text-decoration: none;
   font-size: 24px;
-
   transition: transform .2s ease;
-  /* border-bottom: 2px solid #00000033; */
+  border: none; /* Remove any border if present */
+  padding: 0; /* Remove padding */
+  margin: 0; /* Remove margin if not needed, otherwise adjust as needed */
 
+  position: relative;
+  background: ${(props) => props.$background};
+  max-height: 100%;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-grow: 0;
+  flex-shrink: 0;
+  background-size: 100% auto;
+  background-position: center;
+  font-weight: bold;
 
   & > .image {
     position: absolute;
@@ -54,19 +61,6 @@ const StyledGameCard = styled(NavLink)<{$small: boolean, $background: string}>`
     }
   }
 
-  position: relative;
-  transform: scale(1);
-  background: ${(props) => props.$background};
-  max-height: 100%;
-  overflow: hidden;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-grow: 0;
-  flex-shrink: 0;
-  background-size: 100% auto;
-  background-position: center;
-  font-weight: bold;
   .play {
     font-size: 14px;
     border-radius: 5px;
@@ -77,24 +71,24 @@ const StyledGameCard = styled(NavLink)<{$small: boolean, $background: string}>`
     bottom: 5px;
     opacity: 0;
     text-transform: uppercase;
-
     backdrop-filter: blur(20px);
   }
+  
   &:hover .play {
     opacity: 1;
   }
+  
   &:hover {
     outline: #9564ff33 solid 5px;
     outline-offset: 0px;
   }
 `
 
-export function GameCard({ game }: {game: GameBundle}) {
+export function GameCard({ game }: { game: GameBundle }) {
   const small = useLocation().pathname !== '/'
   return (
     <StyledGameCard
       to={'/' + game.id}
-      $small={small ?? false}
       $background={game.meta?.background}
     >
       <div className="background" />
