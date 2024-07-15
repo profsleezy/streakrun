2bd4a9bbf17b31fd608d75537b24c4d543cc4421
SL55ZY <65545295+profsleezy@users.noreply.github.com>
2024-07-15 07:35:57 +0300

    Update WelcomeBanner.tsx

--- src/sections/Dashboard/WelcomeBanner.tsx
+++ src/sections/Dashboard/WelcomeBanner.tsx
@@ -1,5 +1,8 @@
+import React from 'react'
+import styled from 'styled-components'
 import React, { useState, useEffect } from 'react'
 import styled, { keyframes } from 'styled-components'
+
 
 const fadeIn = keyframes`
   from {
