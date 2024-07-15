66f46637884058e7e30756e1c927babb647c3e76
SL55ZY <65545295+profsleezy@users.noreply.github.com>
2024-07-15 08:19:14 +0300

    bannerimage

--- src/sections/Dashboard/WelcomeBanner.tsx
+++ src/sections/Dashboard/WelcomeBanner.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import bannerImage from 'public/premium'
+import bannerImage from 'public/premium.png'
 
 const Buttons = styled.div`
   overflow: hidden;
