75fd9a3eb352d02936ef6bd1b58440becb84903e
SL55ZY <65545295+profsleezy@users.noreply.github.com>
2024-07-16 11:56:27 +0300

    hell ya

--- src/sections/Dashboard/WelcomeBanner.tsx
+++ src/sections/Dashboard/WelcomeBanner.tsx
@@ -13,11 +13,11 @@ const BannerImages = [
 
 const SlideshowContainer = styled.div`
   position: relative;
-  width: 120%; /* Adjust width as needed */
+  width: 140%; /* Adjust width as needed */
   height: 60vh; /* Adjust height as needed */
   overflow: hidden;
   box-shadow: 0px 6px 10px rgba(0, 0, 0, 0.1);
-  margin: 0 -10%; /* Adjust negative margin to equally expand left and right */
+  margin: 0 -20%; /* Adjust negative margin to equally expand left and right */
 `;
 
 const BannerImage = styled.img`
