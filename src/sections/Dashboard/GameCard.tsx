d24cc61f0b1e27c8c2712542807a52611bfd6dfd
SL55ZY <65545295+profsleezy@users.noreply.github.com>
2024-07-16 17:22:06 +0300

    ??

--- src/sections/Dashboard/GameCard.tsx
+++ src/sections/Dashboard/GameCard.tsx
@@ -1,16 +1,16 @@
-import { GameBundle } from 'gamba-react-ui-v2'
-import React from 'react'
-import { NavLink, useLocation } from 'react-router-dom'
-import styled, { keyframes } from 'styled-components'
+import { GameBundle } from 'gamba-react-ui-v2';
+import React from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
+import styled, { keyframes } from 'styled-components';
 
 const tileAnimation = keyframes`
   0% {
-  background-position: -100px 100px;
+    background-position: -100px 100px;
   }
   100% {
     background-position: 100px -100px;
   }
-`
+`;
 
 const StyledGameCard = styled(NavLink)<{$small: boolean, $background: string}>`
   width: 100%;
@@ -18,18 +18,18 @@ const StyledGameCard = styled(NavLink)<{$small: boolean, $background: string}>`
   @media (min-width: 800px) {
     width: 100%;
   }
-  aspect-ratio: ${(props) => props.$small ? '1/.5' : '1/.6'};
+
+  aspect-ratio: ${(props) => props.$small ? '1/.4' : '1/.5'}; /* Updated aspect ratio */
   background-size: cover;
   border-radius: 10px;
-  
+
   color: white;
   text-decoration: none;
   font-size: 24px;
 
   transition: transform .2s ease;
-  /* border-bottom: 2px solid #00000033; */
 
- & > .background {
+  & > .background {
     position: absolute;
     left: 0;
     top: 0;
@@ -43,10 +43,11 @@ const StyledGameCard = styled(NavLink)<{$small: boolean, $background: string}>`
     animation: ${tileAnimation} 5s linear infinite;
     opacity: 0;
   }
+
   & > .image {
     position: absolute;
-    left: 20;
-    top: 20;
+    left: 0; /* Adjusted for full coverage */
+    top: 0; /* Adjusted for full coverage */
     width: 100%;
     height: 100%;
     background-size: 100% auto;
@@ -78,6 +79,7 @@ const StyledGameCard = styled(NavLink)<{$small: boolean, $background: string}>`
   background-size: 100% auto;
   background-position: center;
   font-weight: bold;
+
   .play {
     font-size: 14px;
     border-radius: 5px;
@@ -88,20 +90,21 @@ const StyledGameCard = styled(NavLink)<{$small: boolean, $background: string}>`
     bottom: 5px;
     opacity: 0;
     text-transform: uppercase;
-
     backdrop-filter: blur(20px);
   }
+
   &:hover .play {
     opacity: 1;
   }
+
   &:hover {
     outline: #9564ff33 solid 5px;
     outline-offset: 0px;
   }
-`
+`;
 
-export function GameCard({ game }: {game: GameBundle}) {
-  const small = useLocation().pathname !== '/'
+export function GameCard({ game }: { game: GameBundle }) {
+  const small = useLocation().pathname !== '/';
   return (
     <StyledGameCard
       to={'/' + game.id}
@@ -112,5 +115,5 @@ export function GameCard({ game }: {game: GameBundle}) {
       <div className="image" style={{ backgroundImage: `url(${game.meta.image})` }} />
       <div className="play">Play {game.meta.name}</div>
     </StyledGameCard>
-  )
+  );
 }
