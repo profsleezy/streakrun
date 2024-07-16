30fc90108a30f744705263f9b2a23749ce266347
SL55ZY <65545295+profsleezy@users.noreply.github.com>
2024-07-16 13:30:23 +0300

    huh

--- src/sections/Dashboard/Dashboard.tsx
+++ src/sections/Dashboard/Dashboard.tsx
@@ -6,10 +6,10 @@ import { GameCard } from './GameCard';
 import { WelcomeBanner } from './WelcomeBanner';
 import { Sidebar } from './sidebar'; // Make sure to import the Sidebar
 
-const MainContent = styled.div
+const MainContent = styled.div`
   margin-left: 50px; /* Adjust this value to match the sidebar's width */
   padding: 1rem;
-;
+`;
 
 export function GameSlider() {
   return (
@@ -23,7 +23,7 @@ export function GameSlider() {
   );
 }
 
-const Grid = styled.div
+const Grid = styled.div`
   display: grid;
   gap: 1rem;
   grid-template-columns: repeat(2, minmax(0, 1fr));
@@ -36,7 +36,7 @@ const Grid = styled.div
   @media (min-width: 1200px) {
     grid-template-columns: repeat(4, minmax(0, 1fr));
   }
-;
+`;
 
 export function GameGrid() {
   return (
