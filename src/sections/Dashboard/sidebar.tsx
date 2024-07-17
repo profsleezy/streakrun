efecd28d4cc808012be05cad63ac87509142944b
SL55ZY <65545295+profsleezy@users.noreply.github.com>
2024-07-18 00:27:52 +0300

    ??

--- src/sections/Dashboard/sidebar.tsx
+++ src/sections/Dashboard/sidebar.tsx
@@ -15,7 +15,7 @@ const SidebarContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 3rem 0 2rem;
+  padding: 4rem 0 3rem;
   box-shadow: 2px 0 5px rgba(0, 0, 0, 0.1);
   font-family: 'Orbitron', sans-serif; /* Use a tech display font */
 `;
