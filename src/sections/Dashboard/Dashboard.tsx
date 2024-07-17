d8cdfa7b7ecc5fd3cc9e4454f1a6e4aeb2a97120
SL55ZY <65545295+profsleezy@users.noreply.github.com>
2024-07-17 23:09:53 +0300

    ??

--- src/sections/Dashboard/Dashboard.tsx
+++ src/sections/Dashboard/Dashboard.tsx
@@ -6,29 +6,22 @@ import { GameCard } from './GameCard';
 import { WelcomeBanner } from './WelcomeBanner';
 import { Sidebar } from './sidebar'; // Make sure to import the Sidebar
 
-const MainContent = styled.div`
+const MainContent = styled.div
   margin-left: 50px; /* Adjust this value to match the sidebar's width */
   padding: 1rem;
   width: 90%;
   margin: 0 auto; /* Center align the content */
-  z-index: 1; /* Ensure main content is above the background */
-`;
+;
 
-const BannerContainer = styled.div`
+const BannerContainer = styled.div
   margin-bottom: 2rem; /* Adjust as needed for spacing */
-`;
+;
 
-const GamesHeading = styled.h2`
+const GamesHeading = styled.h2
   margin-left: 50%; /* Adjust the left margin */
   margin-top: 2rem; /* Adjust the top margin */
   margin-bottom: 1rem; /* Adjust the bottom margin */
-`;
-
-const Grid = styled.div`
-  display: grid;
-  gap: 2rem;
-  grid-template-columns: repeat(3, 1fr); /* Adjusted to 3 columns for larger cards */
-`;
+;
 
 export function GameSlider() {
   return (
@@ -41,6 +34,12 @@ export function GameSlider() {
     </SlideSection>
   );
 }
+
+const Grid = styled.div
+  display: grid;
+  gap: 2rem;
+  grid-template-columns: repeat(3, 1fr); /* Adjusted to 3 columns for larger cards */
+;
 
 export function GameGrid() {
   return (
